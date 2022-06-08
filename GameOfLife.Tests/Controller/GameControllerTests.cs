f5cfa07bb493d3bcfeae5614658c12641589f8ee
Vinh Vu <vinh.vu65@gmail.com>
2022-06-09 09:47:33 +1000

    Rename GameBoard to World

--- GameOfLife.Tests/Controller/GameControllerTests.cs
+++ GameOfLife.Tests/Controller/GameControllerTests.cs
@@ -20,12 +20,12 @@ public class GameControllerTests
     [Theory]
     [InlineData(10, 10)]
     [InlineData(5, 15)]
-    public void CreateGameBoard_ShouldReturnGameBoard_WhenUserInputsSize(int rows, int columns)
+    public void CreateWorld_ShouldReturnWorldGrid_WhenUserInputsSize(int rows, int columns)
     {
-        var expected = new GameBoard(rows, columns);
+        var expected = new World(rows, columns);
         _reader.Read().Returns($"{rows}", $"{columns}");
 
-        var result = _sut.CreateGameBoard();
+        var result = _sut.CreateWorld();
         
         Assert.Equal(expected.Rows, result.Rows);
         Assert.Equal(expected.Columns, result.Columns);
