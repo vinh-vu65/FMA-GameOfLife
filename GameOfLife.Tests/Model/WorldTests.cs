e25d5958be160e7970462dd8623e523d149dd9db
Vinh Vu <vinh.vu65@gmail.com>
2022-06-09 11:36:33 +1000

    Test, Red - World should populate with grid cells when constructed

---
+++ GameOfLife.Tests/Model/WorldTests.cs
@@ -0,0 +1,24 @@
+using GameOfLife.Code.Model;
+using Xunit;
+
+namespace GameOfLife.Tests.Model;
+
+public class WorldTests
+{
+    private readonly World _sut;
+    private readonly int _rows = 10;
+    private readonly int _columns = 15;
+
+    public WorldTests()
+    {
+        _sut = new World(_rows, _columns);
+    }
+    
+    [Fact]
+    public void World_ShouldPopulateWithCells_WhenConstructed()
+    {
+        var expectedPopulation = 150;
+        
+        Assert.Collection(_sut.Population, expectedPopulation);
+    }
+}
