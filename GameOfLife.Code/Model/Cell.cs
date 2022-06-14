2955665ed067983b6ec2d9898176a6e21cd8f42e
Vinh Vu <vinh.vu65@gmail.com>
2022-06-14 15:26:51 +1000

    Rename bool property to IsAlive

--- GameOfLife.Code/Model/Cell.cs
+++ GameOfLife.Code/Model/Cell.cs
@@ -2,7 +2,7 @@ namespace GameOfLife.Code.Model;
 
 public class Cell
 {
-    public bool Alive { get; set; }
+    public bool IsAlive { get; set; }
     public int X { get; }
     public int Y { get; }
 
