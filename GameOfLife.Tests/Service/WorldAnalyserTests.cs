6656f4f293b9e92ce7e0d0fa8da02d924ab285bf
Vinh Vu <vinh.vu65@gmail.com>
2022-06-14 15:26:38 +1000

    Add test cases for wrapping around edges, green

--- GameOfLife.Tests/Service/WorldAnalyserTests.cs
+++ GameOfLife.Tests/Service/WorldAnalyserTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using GameOfLife.Code.Model;
 using GameOfLife.Code.Service;
 using Xunit;
@@ -20,8 +21,89 @@ public class WorldAnalyserTests
     public void GetNeighbours_ShouldReturnEightCellCoordinates_WhenGivenCellCoordinates()
     {
         var expectedLength = 8;
+        
         var result = _sut.GetNeighbours(2,1);
         
         Assert.Equal(expectedLength, result.Count);
+    }
+
+    [Fact]
+    public void GetNeighbours_ShouldReturnEightOrthogonalCells_WhenGivenCellCoordinatesInMiddleOfWorld()
+    {
+        var expected = new List<Cell>
+        {
+            _world[0, 1],
+            _world[0, 2],
+            _world[0, 3],
+            _world[1, 1],
+            _world[1, 3],
+            _world[2, 1],
+            _world[2, 2],
+            _world[2, 3]
+        };
+        
+        var result = _sut.GetNeighbours(2, 1);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetNeighbours_ShouldReturnEightCellsAndWrapAroundToBottomEdge_WhenGivenCellIsOnTopEdgeOfTheWorld()
+    {
+        var expected = new List<Cell>
+        {
+            _world[5, 1],
+            _world[5, 2],
+            _world[5, 3],
+            _world[0, 1],
+            _world[0, 3],
+            _world[1, 1],
+            _world[1, 2],
+            _world[1, 3]
+        };
+        
+        var result = _sut.GetNeighbours(2, 0);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetNeighbours_ShouldReturnEightCellsAndWrapAroundToRightEdge_WhenGivenCellIsOnLeftEdgeOfTheWorld()
+    {
+        var expected = new List<Cell>
+        {
+            _world[1, 5],
+            _world[1, 0],
+            _world[1, 1],
+            _world[2, 5],
+            _world[2, 1],
+            _world[3, 5],
+            _world[3, 0],
+            _world[3, 1]
+        };
+        
+        var result = _sut.GetNeighbours(0, 2);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetNeighbours_ShouldReturnEightCellsAndWrapAroundEdges_WhenGivenCellIsOnCornerOfTheWorld()
+    {
+        var expected = new List<Cell>
+        {
+            _world[5, 5],
+            _world[5, 0],
+            _world[5, 1],
+            _world[0, 5],
+            _world[0, 1],
+            _world[1, 5],
+            _world[1, 0],
+            _world[1, 1]
+        };
+        
+        var result = _sut.GetNeighbours(0, 0);
+        
+        Assert.Equal(expected, result);
     }
 }
