9d7c889e5e34547242e274c0eae0831591ee49ba
Vinh Vu <vinh.vu65@gmail.com>
2022-06-16 15:40:53 +1000

    Green - Cell is added to list if it is dead and contains exactly 3 neighbours

--- GameOfLife.Code/Controller/WorldController.cs
+++ GameOfLife.Code/Controller/WorldController.cs
@@ -1,4 +1,3 @@
-using GameOfLife.Code.IO;
 using GameOfLife.Code.Model;
 using GameOfLife.Code.Service;
 
@@ -24,11 +23,31 @@ public class WorldController
         return output;
     }
 
-    public void BringCellsToLife(List<Cell> cells, World world)
+    private void BringCellsToLife(List<Cell> cells, World world)
     {
         foreach (var cell in cells)
         {
             world.SetCellToAlive(cell.X, cell.Y);
         }
+    }
+
+    public List<Cell> DetermineNextGeneration(Cell[,] world)
+    {
+        var output = new List<Cell>();
+        var rows = world.GetLength(0);
+        var columns = world.GetLength(1);
+        for (var i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                var neighbours = _analyser.GetNeighbours(j, i, world);
+                var liveNeighbours = _analyser.CountAliveCells(neighbours);
+                if (!world[i,j].IsAlive && liveNeighbours != 3) continue;
+                
+                output.Add(world[i,j]);
+            }
+        }
+
+        return output;
     }
 }
