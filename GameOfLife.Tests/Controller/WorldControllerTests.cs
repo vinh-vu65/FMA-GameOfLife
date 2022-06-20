53f78db22b22b03da9d5898865ee0b6fc33c93c1
Vinh Vu <vinh.vu65@gmail.com>
2022-06-20 13:16:46 +1000

    Refactor, WorldController uses IWorldAnalyser instead of concrete class

--- GameOfLife.Tests/Controller/WorldControllerTests.cs
+++ GameOfLife.Tests/Controller/WorldControllerTests.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using GameOfLife.Code.Controller;
 using GameOfLife.Code.Model;
+using GameOfLife.Code.Service;
+using NSubstitute;
 using Xunit;
 
 
@@ -9,30 +11,31 @@ namespace GameOfLife.Tests.Controller;
 public class WorldControllerTests
 {
     private readonly WorldController _sut;
+    private readonly IWorldAnalyser _analyser = Substitute.For<IWorldAnalyser>();
     private readonly int _rows = 6;
     private readonly int _columns = 6;
 
     public WorldControllerTests()
     {
-        _sut = new WorldController(_rows, _columns);
+        _sut = new WorldController(_rows, _columns, _analyser);
     }
     
     [Theory]
     [InlineData(10, 10)]
     [InlineData(5, 15)]
-    public void CreateWorld_ShouldReturnWorldGridOfGivenDimensions_WhenDimensionsAreGivenInConstructor(int rows, int columns)
+    public void CreateWorldPopulation_ShouldReturnWorldGridOfGivenDimensions_WhenDimensionsAreGivenInConstructor(int rows, int columns)
     {
-        var sut = new WorldController(rows, columns);
+        var sut = new WorldController(rows, columns, _analyser);
         var emptyList = new List<Cell>();
         
-        var result = sut.CreateWorld(emptyList);
+        var result = sut.CreateWorldPopulation(emptyList);
         
-        Assert.Equal(rows, result.Population.GetLength(0));
-        Assert.Equal(columns, result.Population.GetLength(1));
+        Assert.Equal(rows, result.GetLength(0));
+        Assert.Equal(columns, result.GetLength(1));
     }
 
     [Fact]
-    public void CreateWorld_ShouldChangeGivenCellsToAliveInWorldOutput_WhenGivenListOfCells()
+    public void CreateWorldPopulation_ShouldChangeGivenCellsToAliveInOutput_WhenGivenListOfCells()
     {
         var cellsList = new List<Cell>
         {
@@ -41,27 +44,27 @@ public class WorldControllerTests
             new(0, 0)
         };
 
-        var result = _sut.CreateWorld(cellsList);
+        var result = _sut.CreateWorldPopulation(cellsList);
 
-        Assert.True(result.Population[1,2].IsAlive);
-        Assert.True(result.Population[2,3].IsAlive);
-        Assert.True(result.Population[0,0].IsAlive);
-        Assert.False(result.Population[5,5].IsAlive);
+        Assert.True(result[1,2].IsAlive);
+        Assert.True(result[2,3].IsAlive);
+        Assert.True(result[0,0].IsAlive);
+        Assert.False(result[5,5].IsAlive);
     }
 
     [Fact]
     public void DetermineNextGeneration_ShouldIncludeCell_WhenGivenCellIsNotAliveAndHasExactlyThreeLiveNeighbours()
     {
-        var aliveList = new List<Cell>
+        var world = _sut.CreateWorldPopulation(new List<Cell>());
+        var neighbours = new List<Cell>
         {
-            new(1, 0) {IsAlive = true},
-            new(1, 2) {IsAlive = true},
-            new(3, 0) {IsAlive = true}
+            new(1, 1)
         };
-        var world = _sut.CreateWorld(aliveList);
-        var expected = world.Population[1, 2];
+        _analyser.GetNeighbours(2, 1, world).Returns(neighbours);
+        _analyser.CountAliveCells(neighbours).Returns(3);
+        var expected = world[1, 2];
 
-        var result = _sut.DetermineNextGeneration(world.Population);
+        var result = _sut.DetermineNextGeneration(world);
 
         Assert.Contains(expected, result);
         Assert.Single(result);
@@ -70,16 +73,20 @@ public class WorldControllerTests
     [Fact]
     public void DetermineNextGeneration_ShouldIncludeCell_WhenGivenCellIsAliveAndHasTwoLiveNeighbours()
     {
-        var aliveList = new List<Cell>
+        var liveCell = new List<Cell>
         {
-            new(3, 2) {IsAlive = true},
-            new(1, 0) {IsAlive = true},
             new(2, 1) {IsAlive = true}
         };
-        var world = _sut.CreateWorld(aliveList);
-        var expected = world.Population[1, 2];
+        var world = _sut.CreateWorldPopulation(liveCell);
+        var neighbours = new List<Cell>
+        {
+            new(1, 1)
+        };
+        _analyser.GetNeighbours(2, 1, world).Returns(neighbours);
+        _analyser.CountAliveCells(neighbours).Returns(2);
+        var expected = world[1, 2];
 
-        var result = _sut.DetermineNextGeneration(world.Population);
+        var result = _sut.DetermineNextGeneration(world);
 
         Assert.Contains(expected, result);
         Assert.Single(result);
@@ -88,20 +95,22 @@ public class WorldControllerTests
     [Fact]
     public void DetermineNextGeneration_ShouldIncludeCell_WhenGivenCellIsAliveAndHasThreeLiveNeighbours()
     {
-        var aliveList = new List<Cell>
+        var liveCell = new List<Cell>
         {
-            new(1, 0) {IsAlive = true},
-            new(1, 2) {IsAlive = true},
-            new(3, 0) {IsAlive = true},
             new(2, 1) {IsAlive = true}
         };
-        var world = _sut.CreateWorld(aliveList);
-        var expected = world.Population[1, 2];
-        var expectedLength = 3;
+        var world = _sut.CreateWorldPopulation(liveCell);
+        var neighbours = new List<Cell>
+        {
+            new(1, 1)
+        };
+        _analyser.GetNeighbours(2, 1, world).Returns(neighbours);
+        _analyser.CountAliveCells(neighbours).Returns(3);
+        var expected = world[1, 2];
 
-        var result = _sut.DetermineNextGeneration(world.Population);
+        var result = _sut.DetermineNextGeneration(world);
 
         Assert.Contains(expected, result);
-        Assert.Equal(expectedLength, result.Count);
+        Assert.Single(result);
     }
 }
