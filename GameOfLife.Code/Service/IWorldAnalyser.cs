ca1689aed3f90717cc0fa3896d2c0afeaa7092c7
Vinh Vu <vinh.vu65@gmail.com>
2022-06-21 11:53:53 +1000

    Update method required by interface

--- GameOfLife.Code/Service/IWorldAnalyser.cs
+++ GameOfLife.Code/Service/IWorldAnalyser.cs
@@ -4,6 +4,5 @@ namespace GameOfLife.Code.Service;
 
 public interface IWorldAnalyser
 {
-    List<Cell> GetNeighbours(int x, int y, Cell[,] grid);
-    int CountAliveCells(List<Cell> neighbours);
+    List<Cell> DetermineNextGeneration(Cell[,] grid);
 }
