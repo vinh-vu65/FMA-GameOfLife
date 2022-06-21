063dff045be53bd8f22b037ddc1953340167fc2c
Vinh Vu <vinh.vu65@gmail.com>
2022-06-21 11:26:25 +1000

    Green - SeedGenerator reads from txt file

--- GameOfLife.Code/Service/SeedGenerator.cs
+++ GameOfLife.Code/Service/SeedGenerator.cs
@@ -2,5 +2,10 @@ namespace GameOfLife.Code.Service;
 
 public class SeedGenerator
 {
+    public string[] ReadFile(string filePath)
+    {
+        return File.ReadAllLines($"../../../{filePath}");
+    }
+    
     
 }
--- GameOfLife.Tests/Service/SeedGeneratorTests.cs
+++ GameOfLife.Tests/Service/SeedGeneratorTests.cs
@@ -6,20 +6,30 @@ namespace GameOfLife.Tests.Service;
 public class SeedGeneratorTests
 {
     private readonly SeedGenerator _sut;
+    private readonly string _filePath;
 
     public SeedGeneratorTests()
     {
         _sut = new SeedGenerator();
+        _filePath = "test.txt";
     }
     
     [Fact]
     public void ReadFile_ShouldReturnStringArray_WhenGivenFilePath()
     {
-        var testFilePath = "test.txt";
         var expectedLength = 10;
         
-        var result = _sut.ReadFile(testFilePath);
+        var result = _sut.ReadFile(_filePath);
         
         Assert.Equal(expectedLength, result.Length);
+    }
+
+    [Fact]
+    public void GetWorldDimensions_ShouldSetRowsAndColumns_WhenTxtFileHasXCharacterOnBottomRightCorner()
+    {
+        var expectedRows = 10;
+        var expectedColumns = 50;
+
+        _sut.GetWorldDimensions();
     }
 }
