071b3f3ff31ae48bcca1ed3a296699e24a9f9fde
Vinh Vu <vinh.vu65@gmail.com>
2022-06-21 11:53:26 +1000

    Green - WorldDimensions correctly set with * in bottom right corner

--- GameOfLife.Code/Service/SeedGenerator.cs
+++ GameOfLife.Code/Service/SeedGenerator.cs
@@ -2,10 +2,18 @@ namespace GameOfLife.Code.Service;
 
 public class SeedGenerator
 {
+    public int Rows { get; private set; }
+    public int Columns { get; private set; }
+    
     public string[] ReadFile(string filePath)
     {
         return File.ReadAllLines($"../../../{filePath}");
     }
-    
-    
+
+    public void SetWorldDimensions(string[] seed)
+    {
+        Rows = seed.Length;
+        var lastLine = seed[Rows - 1];
+        Columns = lastLine.IndexOf('*') + 1;
+    }
 }
