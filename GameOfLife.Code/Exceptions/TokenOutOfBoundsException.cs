6c5e9d73afa6d52932ea414b0e2b3ac4ad4ea9dd
Vinh Vu <vinh.vu65@gmail.com>
2022-06-22 13:09:18 +1000

    Formatting

--- GameOfLife.Code/Exceptions/TokenOutOfBoundsException.cs
+++ GameOfLife.Code/Exceptions/TokenOutOfBoundsException.cs
@@ -2,7 +2,7 @@ namespace GameOfLife.Code.Exceptions;
 
 public class TokenOutOfBoundsException : Exception
 {
-    public TokenOutOfBoundsException(int y) : base(message: $"Seed file has a token beyond bounds set by <*> in file on Row {y+1}. " +
-                                                       "Ensure all tokens are within bounds and try again.")
+    public TokenOutOfBoundsException(int y) : base(message:$"Seed file has a token beyond bounds set by asterisk <*> in " +
+                                                            $"file on Row {y+1}. Ensure all tokens are within bounds and try again.")
     { }
 }
