3b046b37bc915aad892e19d7db76498bda3d371e
Vinh Vu <vinh.vu65@gmail.com>
2022-06-22 10:55:15 +1000

    Rename SeedGenerator to Seed Parser

--- GameOfLife.Code/Exceptions/TokenOutOfBoundsException.cs
+++ GameOfLife.Code/Exceptions/TokenOutOfBoundsException.cs
@@ -2,7 +2,7 @@ namespace GameOfLife.Code.Exceptions;
 
 public class TokenOutOfBoundsException : Exception
 {
-    public TokenOutOfBoundsException(int y) : base(message: $"Seed file has a token beyond bounds set by <*> in file on line {y+1}. " +
+    public TokenOutOfBoundsException(int y) : base(message: $"Seed file has a token beyond bounds set by <*> in file on Row {y+1}. " +
                                                        "Ensure all tokens are within bounds and try again.")
     { }
 }
