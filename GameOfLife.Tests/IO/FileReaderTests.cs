e35d0b9771a338db7c859ff7ca32e7f0b98dddd9
Vinh Vu <vinh.vu65@gmail.com>
2022-06-27 14:04:26 +1000

    Move test from seed parser to file reader

---
+++ GameOfLife.Tests/IO/FileReaderTests.cs
@@ -0,0 +1,28 @@
+using GameOfLife.Code.IO;
+using Xunit;
+
+namespace GameOfLife.Tests.IO;
+
+public class FileReaderTests
+{
+    [Fact]
+    public void Read_ShouldReturnAStringArray_WhenConstructorIsGivenAFileNameInSeedsFolder()
+    {
+        var fileName = "testSeed.txt";
+        var sut = new FileReader(fileName);
+        var expected = new[]
+        {
+            "###",
+            "",
+            "",
+            " #",
+            "####",
+            " #",
+            "    *"
+        };
+
+        var result = sut.Read();
+        
+        Assert.Equal(expected, result);
+    }
+}
