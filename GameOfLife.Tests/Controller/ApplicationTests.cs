12714089abd2fe2d43e03f695548ceb31d2d95ee
Vinh Vu <vinh.vu65@gmail.com>
2022-06-28 14:29:40 +1000

    Add application layer with tests

--- GameOfLife.Tests/Controller/ApplicationTests.cs
+++ GameOfLife.Tests/Controller/ApplicationTests.cs
@@ -1,5 +1,6 @@
 using GameOfLife.Code.Controller;
 using GameOfLife.Code.IO;
+using GameOfLife.Code.Model;
 using NSubstitute;
 using Xunit;
 
@@ -8,20 +9,33 @@ namespace GameOfLife.Tests.Controller;
 public class ApplicationTests
 {
     private readonly Application _sut;
-    private readonly IReader _reader = Substitute.For<IReader>();
+    private readonly IWorldRenderer _renderer = Substitute.For<IWorldRenderer>();
     private readonly IWriter _writer = Substitute.For<IWriter>();
     private readonly IGameService _gameService = Substitute.For<IGameService>();
 
     public ApplicationTests()
     {
-        _sut = new Application(_reader, _writer, _gameService);
+        _sut = new Application(_writer, _gameService, _renderer);
     }
 
     [Fact]
-    public void Setup_ShouldCallGameServiceGetSeedMethod()
+    public void Run_ShouldRenderWorldWriteRenderedWorldAndTickGameService()
     {
-        _sut.Setup();
-
-        _gameService.Received(1).GetSeed();
+        _sut.Run(1);
+        
+        _writer.Received().Write(Arg.Any<string>());
+        _renderer.Received().Render(Arg.Any<World>());
+        _gameService.Received().Tick();
+    }
+    
+    [Fact]
+    public void Run_ShouldRepeatServiceMethodCalls_WhenGivenNumberOfTimesToRepeat()
+    {
+        var timesToRepeat = 3;
+        _sut.Run(timesToRepeat);
+        
+        _writer.Received(timesToRepeat).Write(Arg.Any<string>());
+        _renderer.Received(timesToRepeat).Render(Arg.Any<World>());
+        _gameService.Received(timesToRepeat).Tick();
     }
 }
