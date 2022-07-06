97a48b87788023c4a30e359763e6b39e1c714e9a
Vinh Vu <vinh.vu65@gmail.com>
2022-07-06 12:08:05 +1000

    Add static class for all game messages

--- GameOfLife.Code/IO/GameMessageBuilder.cs
+++ GameOfLife.Code/IO/GameMessageBuilder.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using System.Text;
 
 namespace GameOfLife.Code.IO;
@@ -7,18 +6,19 @@ public static class GameMessageBuilder
 {
     public static string StartupMessage()
     {
-        return "Welcome to Conway's Game Of Life!";
+        return "Welcome to Conway's Game Of Life!\n";
     }
 
-    public static string ChooseSeed()
+    public static string ChooseSeed(string[] seedFiles)
     {
-        var seedFiles = GetSeedFileNames();
         var sb = new StringBuilder();
-        sb.Append($"Please choose a seed file to load, enter a number from [1 - {seedFiles.Length}:\n");
+        sb.Append("Please choose a seed file to load\n");
         for (var i = 0; i < seedFiles.Length; i++)
         {
             sb.Append($"{i + 1}. {seedFiles[i]}\n");
         }
+
+        sb.Append($"Enter a number from [1 - {seedFiles.Length}]: ");
 
         return sb.ToString();
     }
@@ -26,18 +26,40 @@ public static class GameMessageBuilder
     public static string ChooseSpeed()
     {
         var sb = new StringBuilder();
-        sb.Append("Please set the speed for the simulations to run, enter a number from [1 - 4]:");
-        sb.Append("1. Slow\n2. Regular\n3. Fast\n 4. No Delay");
+        sb.Append("\nPlease set the speed for the simulations to run\n");
+        sb.Append("1. Slow\n2. Regular\n3. Fast\n4. No Delay\n");
+        sb.Append("Enter a number from [1 - 4]: ");
 
         return sb.ToString();
     }
 
-    private static string[] GetSeedFileNames()
+    public static string ChooseRepetitions()
     {
-        var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Seeds");
-        var d = new DirectoryInfo(path);
-        var fileArray = d.GetFiles();
+        var sb = new StringBuilder();
+        sb.Append("\nPlease choose the maximum number of generations you would like to simulate\n");
+        sb.Append("The simulation will run for your selected number of generations or until the world has stabilised\n");
+        sb.Append("Please enter a number: ");
 
-        return fileArray.Select(x => x.Name).ToArray();
+        return sb.ToString();
+    }
+
+    public static string InvalidInput()
+    {
+        return "Please enter a valid number: ";
+    }
+
+    public static string DisplayGeneration(int generation)
+    {
+        return $"Generation: {generation}";
+    }
+
+    public static string SimulationEnded()
+    {
+        return "Simulation has ended!";
+    }
+
+    public static string WorldStabilised(int generation)
+    {
+        return $"The world has stabilised at generation {generation}.";
     }
 }
